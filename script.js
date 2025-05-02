394663c1d02789a8f7b310e18ffbe1c82b3a3779
vinzz-officials <vinzz4358@gmail.com>
2025-05-02 08:48:46 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
-function openTab(tabId, element) {
-  // Sembunyikan semua tab
+
+function openTab(tabId, btn) {
   const contents = document.querySelectorAll('.tab-content');
-  contents.forEach(c => c.classList.remove('active-tab'));
-
-  // Hapus active class dari tombol
   const buttons = document.querySelectorAll('.tabs button');
-  buttons.forEach(b => b.classList.remove('active'));
 
-  // Tampilkan tab yang diklik
-  document.getElementById(tabId).classList.add('active-tab');
-  element.classList.add('active');
+  contents.forEach(content => {
+    if (content.id === tabId) {
+      content.classList.add('active-tab');
+    } else {
+      content.classList.remove('active-tab');
+    }
+  });
+
+  buttons.forEach(b => b.classList.remove('active'));
+  btn.classList.add('active');
 }
+  
